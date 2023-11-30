073b3895c44176aa0094fdbdedbae69d65c22617
Glayson Visgueira <glaysonwow@gmail.com>
2023-11-30 13:04:38 -0300

    adicao de title tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import GlobalStyles from "@/styles/GlobalStyles";
+import Head from "next/head";
 
 import Layout from "@/components/templates/Layout";
 import ThemeContext from "@/context/ThemeContext";
@@ -7,6 +8,14 @@ import ThemeContext from "@/context/ThemeContext";
 export default function App({ Component, pageProps }) {
   return (
     <ThemeContext>
+      <Head>
+        <title>UFPI - Sistema Fuzzy</title>
+        <meta property="og:title" content="UFPI - Sistema Fuzzy" />
+        <meta property="og:description" content="UFPI - Sistema Fuzzy" />
+        <meta name="theme-color" content="#232323" />
+        <meta name="msapplication-navbutton-color" content="#232323" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#232323" />
+      </Head>
       <GlobalStyles />
       <Layout>
         <Component {...pageProps} />
